5f93adbb111f3c5ee808ea8f3275eda24562e08a
jinhyeok <wlsgur079@gmail.com>
2023-03-21 09:33:08 +0900

    오더페이지 작업

--- RestaurantProject/src/main/java/com/restaurant/controller/OrderController.java
+++ RestaurantProject/src/main/java/com/restaurant/controller/OrderController.java
@@ -1,15 +1,25 @@
 package com.restaurant.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.restaurant.dto.OrderMenuTabDTO;
 import com.restaurant.service.OrderService;
 
 @Controller
@@ -21,13 +31,26 @@ public class OrderController {
 	}
 
 	@RequestMapping("/order")
-	public String orderPage() {
-		return "order/order";
+	public ModelAndView orderPage() {
+		List<OrderMenuTabDTO> hotList = orderService.searchMenuHotTab();
+		List<OrderMenuTabDTO> singleList = orderService.searchMenuOtherTab("A1");
+		List<OrderMenuTabDTO> setList = orderService.searchMenuOtherTab("A2");
+		List<OrderMenuTabDTO> sideList = orderService.searchMenuOtherTab("A3");
+		List<OrderMenuTabDTO> beverageList = orderService.searchMenuOtherTab("A4");
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("order/order"); 
+		mv.addObject("hList", hotList);
+		mv.addObject("singleList", singleList);
+		mv.addObject("setList", setList);
+		mv.addObject("sideList", sideList);
+		mv.addObject("beverageList", beverageList);
+		
+		return mv;
 	}
-	
+	 
 	@RequestMapping("/order/search/office")
 	public ResponseEntity<String> searchCanReservOffice(Date date, int time, String persons) {
-		
 		SimpleDateFormat sdf = new SimpleDateFormat("yyMMdd");
 		String reservDate = sdf.format(date);
 		int searchStartTime = time - 99;
@@ -38,13 +61,61 @@ public class OrderController {
 		} else {
 			searchEndTime = time + 99;
 		}
-				 
+		 
 		List<HashMap<String, Object>> list = orderService.searchCanReservOffice(reservDate, searchStartTime, searchEndTime, persons);
-		
-		for(HashMap<String, Object> t : list) {
-			System.out.println(t.toString());
-		}
 		
 		return new ResponseEntity(list, HttpStatus.OK);
 	}
+	
+	@RequestMapping("/order/search/seatmap")
+	public ResponseEntity<String> searchSeatMap(String code, Date date, int time) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyMMdd");
+		String reservDate = sdf.format(date);
+		int searchStartTime = time - 99;
+		int searchEndTime = 0;
+			
+		if(time%100 == 0) {
+			searchEndTime = time + 59;
+		} else {
+			searchEndTime = time + 99;
+		}
+		
+		List<HashMap<String, Object>> list = orderService.searchSeatmap(code, reservDate, searchStartTime, searchEndTime);
+		
+		return new ResponseEntity(list, HttpStatus.OK);
+	}
+	
+	@RequestMapping("/order/imgdown/{code}")
+	public void searchMenuTab(@PathVariable("code") int code, HttpServletResponse response) {
+		String path = orderService.searchImgPath(code);
+		File file = new File(path);
+		String fileName = "";
+		
+		try {
+			fileName = URLEncoder.encode(path.toLowerCase(),"utf-8");
+		} catch (UnsupportedEncodingException e1) {
+			e1.printStackTrace();
+		}
+		response.setHeader("Content-Disposition", "attachement;fileName=" + fileName);
+		response.setHeader("Content-Transfer-Encoding", "binary");
+		response.setContentLength((int)file.length());
+
+		try(FileInputStream fis = new FileInputStream(file);
+				BufferedOutputStream bos = new BufferedOutputStream(response.getOutputStream())){
+			
+			byte[] buffer = new byte[1024*1024];
+			
+			while(true) {
+				int size = fis.read(buffer);
+				if(size == -1) break;
+				bos.write(buffer,0,size);
+				bos.flush();
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	 
 }
--- RestaurantProject/src/main/java/com/restaurant/mapper/OrderMapper.java
+++ RestaurantProject/src/main/java/com/restaurant/mapper/OrderMapper.java
@@ -5,9 +5,19 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.restaurant.dto.OrderMenuTabDTO;
+
 @Mapper
 public interface OrderMapper {
 
 	List<HashMap<String, Object>> searchCanReservOffice(HashMap<String, Object> map);
+
+	List<HashMap<String, Object>> searchSeatMap(HashMap<String, Object> map);
+
+	List<OrderMenuTabDTO> searchMenuHotTab();
+
+	List<OrderMenuTabDTO> searchMenuOtherTab(String mainCategoryCode);
+
+	String searchImgPath(int code);
 
 }
