146697765a2c170c559bb00aec9db89295a2e80f
hr <fla2641@naver.com>
2023-04-07 17:46:36 +0900

    사용자 faq notice inquiry ajax구현 매니저 로그인 구현 매니저 faq 관리 화면 구현

--- RestaurantProject/src/main/java/com/restaurant/controller/ManagerController.java
+++ RestaurantProject/src/main/java/com/restaurant/controller/ManagerController.java
@@ -1,13 +1,48 @@
 package com.restaurant.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.restaurant.dto.ManagerInfoDTO;
+import com.restaurant.service.ManagerService;
 
 @Controller
 public class ManagerController {
+	private ManagerService managerService;
+	
+		
+	public ManagerController(ManagerService managerService) {
+		this.managerService = managerService;
+	}
+
 	@RequestMapping("/managerLog")
 	public String managerIndex() {
 		
 		return"/managerService/manager_login";
+	}
+	
+	@RequestMapping("/managerLogin")
+	public String managerLogin(String managerId, String managerPW, HttpSession session) {
+		
+		ManagerInfoDTO dto = managerService.managerLogin(managerId, managerPW);
+		session.setAttribute("dto", dto);
+		
+		
+		return"/managerService/manager_welcome";
+	}
+	
+	@RequestMapping("/faqSetting")
+	public String managerFAQSetting() {
+		
+		
+		return "/customer_manager/FAQ_setting";
+	}
+	@RequestMapping("/inqureyList")
+	public String inqureyList() {
+		
+		return null;
 	}
 }
